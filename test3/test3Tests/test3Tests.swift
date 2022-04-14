91578c5cb7bd28723f9e3eba56691a3c0fe259e6
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-04-14 11:39:52 +0100

    Remove unused function

--- test3/test3Tests/test3Tests.swift
+++ test3/test3Tests/test3Tests.swift
@@ -10,14 +10,6 @@ import XCTest
 
 class test3Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testXXX() throws {
         let viewModel = ViewModel()
         
@@ -31,13 +23,6 @@ class test3Tests: XCTestCase {
         wait(for: [expectation], timeout: 5)
         print(viewModel.testModel.count)
         XCTAssert(viewModel.testModel.count == 1)
-    }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
